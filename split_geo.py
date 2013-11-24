1ebdd3e0d80fba40e6227fd6d910c95f972814a6
jackiekazil <jackiekazil@gmail.com>
2013-11-24 00:10:55 -0500

    First round at parsing json.

---
+++ split_geo.py
@@ -0,0 +1,32 @@
+
+
+import json
+import sys
+
+
+data_file = sys.argv[1]  #original file
+save_folder = sys.argv[2]  #parsed/destination
+
+json_data = open(data_file, 'r')
+data = json.load(json_data)
+
+# Define the out data dict. We want crs & type to
+# stay the same, so we will go ahea and store those.
+feature = {}
+
+#[u'geometry', u'type', u'properties']
+count = 0
+for f in data['features']:
+	if count < 1:
+		file_name = f['properties']['DESCRIPTIO']
+		file_name = file_name.replace(' ','-').lower()
+
+		feature['geometry'] = f['geometry']
+		feature['type'] = f['type']
+		feature['properties'] = f['properties']
+	count+=1
+
+	out_file = save_folder + file_name + '.geojson'
+	with open(out_file, 'wb') as fp:
+	    json.dump(feature, fp)
+
